a74c9de77f53783bf2eaaa5bfb2a232bf248e768
mariem-ben-ghozlen <58255315+mariem-ben-ghozlen@users.noreply.github.com>
2022-11-18 10:10:45 +0100

    mariem
    mariem

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true
+    apiUrl: 'http://192.168.1.156:8089/SpringMVC/'
 };
 
 /*
